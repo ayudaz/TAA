f5ef74e9055c91dd80752e78ae97a19a3c14978e
Thomas Lelievre <thomaslelievre72@gmail.com>
2013-10-02 23:53:08 +0200

    TP1: ajout du projet eclipse

---
+++ ws/tpmaven/src/main/java/fr/istic/master2/tpmaven/App.java
@@ -0,0 +1,26 @@
+package fr.istic.master2.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void codeMort(){
+    	if(true){
+    		if(false){
+    			if(true){
+    				System.out.println("TPTPTPPT");
+    			}
+    			else if(false){
+    				int a;
+    			}
+    		}
+    	}
+    }
+}
